34ec5fcc5242adb6006e959774aa82fa79920ae2
hasibul1670 <hasibulislam1670@gmail.com>
2023-02-24 21:38:54 +0600

    Add project files.

---
+++ firstConsoleApp/Program.cs
@@ -0,0 +1,69 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace firstConsoleApp
+{
+
+    
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+
+        {
+
+            int[] arr = new int[5];
+
+
+            int max, min, n;
+            Console.Write("Enter the array elements : ");
+            Console.WriteLine("-----------------------------------------\n");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("element - {0}: ", i);
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+            Console.Write("\nElements in array are: ");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("{0}  ", arr[i]);
+
+            }
+
+            Console.WriteLine( "Hello,max min");
+
+            max = arr[0];
+            min = arr[0];
+            int w = arr.Length;
+            Console.WriteLine("hello : " + w);
+            for (int i = 0; i < w; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+
+            }
+            Console.WriteLine("Max element :" + max);
+            Console.WriteLine("Min element :" + min);
+
+
+
+
+
+            Console.ReadKey();
+
+
+
+        }
+
+    }
+}
